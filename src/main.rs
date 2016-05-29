50d459f47b251ed1ade6cab0cb0cde9456bc45cc
Raphael Nestler <raphael.nestler@gmail.com>
2016-05-29 21:15:33 +0200

    Parse pacman and uname output
    We are only interested in the version of the installed and running
    kernel. So we extract them from the raw output.

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,24 @@ fn main() {
         .arg("linux")
         .output()
         .expect("Could not execute pacman");
+    // pacman output is in the form "linux version"
+    let output_pacman = String::from_utf8_lossy(&output_pacman.stdout);
+    let output_pacman = output_pacman.split_whitespace()
+        .skip(1)
+        .next()
+        .expect("Could not parse pacman output");
 
+    // uname output is in the form version-ARCH
     let output_uname = Command::new("uname")
         .arg("-r")
         .output()
         .expect("Could not execute uname");
-
-    let output_pacman = String::from_utf8_lossy(&output_pacman.stdout);
     let output_uname = String::from_utf8_lossy(&output_uname.stdout);
-    print!("installed: {}running: {}", output_pacman, output_uname);
+    let output_uname = output_uname.split("-ARCH")
+        .next()
+        .expect("Could not parse uname output");
+
+
+    print!("installed: {}\nrunning: {}\n", output_pacman, output_uname);
 }
 
