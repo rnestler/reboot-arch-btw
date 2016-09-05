d080b28ece98b0558e65a175ce8edfd517eb7f8d
Raphael Nestler <raphael.nestler@gmail.com>
2016-09-05 02:08:30 +0200

    Also check version of the xserver

--- src/main.rs
+++ src/main.rs
@@ -35,6 +35,17 @@ fn parse_uname_output(uname_output: &str) -> Option<&str> {
         .next()
 }
 
+
+/// Parse the output of `xdpyinfo`
+fn parse_xdpyinfo_output(xdpyinfo_output: &str) -> Option<&str> {
+    for line in xdpyinfo_output.lines() {
+        if line.starts_with("X.Org version: ") {
+            return Some(line.split_at(15).1);
+        }
+    }
+    None
+}
+
 const USAGE: &'static str = "
 Check the currently installed kernel against the currently running one.
 
@@ -62,7 +73,7 @@ fn main() {
         println!("kernel-updated: {}", env!("CARGO_PKG_VERSION"));
         return;
     }
-    
+
     // uname output is in the form version-ARCH
     let output_uname = Command::new("uname")
         .arg("-r")
@@ -75,8 +86,9 @@ fn main() {
     let installed_kernel_version = get_package_version("linux")
         .expect("Could not get version of installed kernel");
 
-    println!("installed: {}", installed_kernel_version);
-    println!("running:   {}", running_kernel_version);
+    println!("Kernel");
+    println!(" installed: {}", installed_kernel_version);
+    println!(" running:   {}", running_kernel_version);
 
     let should_reboot = installed_kernel_version != running_kernel_version;
 
@@ -88,12 +100,25 @@ fn main() {
             .timeout(6000) //milliseconds
             .show().unwrap();
     }
+
+    let output_xdpyinfo = Command::new("xdpyinfo")
+        .output()
+        .expect("Could not execute xdpyinfo");
+    let output_xdpyinfo = String::from_utf8_lossy(&output_xdpyinfo.stdout);
+    let running_xorg_version = parse_xdpyinfo_output(&output_xdpyinfo)
+        .expect("Could not parse xdpyinfo output");
+    let installed_xorg_version = get_package_version("xorg-server")
+        .expect("Could not get version of installed xserver");
+
+    println!("Xorg server");
+    println!(" installed: {}", installed_xorg_version);
+    println!(" running: {}", running_xorg_version);
 }
 
 
 #[cfg(test)]
 mod test {
-    use super::{parse_pacman_output, parse_uname_output};
+    use super::{parse_pacman_output, parse_uname_output, parse_xdpyinfo_output};
 
     #[test]
     fn test_parse_pacman_output() {
@@ -103,6 +128,11 @@ mod test {
     #[test]
     fn test_parse_uname_output() {
         assert_eq!(Some("4.5.4-1"), parse_uname_output("4.5.4-1-ARCH"));
+    }
+
+    #[test]
+    fn test_parse_xdpyinfo_output() {
+        assert_eq!(Some("1.18.4"), parse_xdpyinfo_output("X.Org version: 1.18.4"));
     }
 }
 
